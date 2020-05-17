005f45bb8af73cd6f6edf9f015573280c5f52d5a
Edi Hasaj <edihasaj@gmail.com>
2020-05-17 02:07:33 +0200

    bug fixes

--- resources/js/config.js
+++ resources/js/config.js
@@ -1,2 +1,2 @@
+export const API_BASE_URL = 'https://epizza.herokuapp.com/api';
 export const LOCAL_API_BASE_URL = 'http://localhost:8000/api';
-export const API_BASE_URL = 'https://epizza.herokuapp.com/';
